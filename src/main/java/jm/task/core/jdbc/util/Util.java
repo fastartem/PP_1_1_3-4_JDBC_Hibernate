03e86b7fefaa891456ad70038c89c9c1c1547c94
Tomikus <tomikus1@gmail.com>
2023-11-09 11:18:06 +0300

    connection modified and hiber config added

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,14 +14,35 @@ public class Util {
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "11111";
 
-    public static Connection getConnection() {
-        Connection connection = null;
+    //JDBC
+    private static Connection connection;
+
+    static {
         try {
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             System.out.println("Connection OK");
         } catch (SQLException e) {
             System.out.println("Connection ERROR");
         }
+    }
+
+    public static Connection getConnection() {
         return connection;
+    }
+
+    //Hibernate
+    private static final SessionFactory sessionFactory;
+
+    static {
+        sessionFactory = new Configuration()
+                .setProperty("hibernate.connection.url", DB_URL)
+                .setProperty("hibernate.connection.username", DB_USERNAME)
+                .setProperty("hibernate.connection.password", DB_PASSWORD)
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
